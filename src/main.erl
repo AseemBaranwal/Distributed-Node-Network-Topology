d721f6104628f9015fdb5c1940d36f0158734b42
AseemBaranwal <aseembaranwal23@gmail.com>
2022-10-08 03:53:04 -0400

    Created Full Topology

--- src/main.erl
+++ src/main.erl
@@ -10,13 +10,10 @@
 -author("aseem").
 -export([start/0]).
 
-%% API
--export([]).
-
 start() ->
-  {ok, NumNodes} = io:read("Enter the number of Nodes: "),
-  {ok, TopologyType} = io:read("Enter the type of topology: "),
-  {ok, Algorithm} = io:read("Enter the algorithm to be used: "),
+  {_, [NumNodes]} = io:fread("Enter the number of Nodes: ", "~d"),
+  {_, [TopologyType]} = io:fread("Enter the type of topology: ", "~s"),
+  {_, [Algorithm]} = io:fread("Enter the algorithm to be used: ", "~s"),
 
   io:fwrite("~n----------------------------------------------------------------------------------~n~n"),
 
@@ -26,8 +23,8 @@ start() ->
   MasterPID ! {spawnProcess, MasterPID, NumNodes}.
 
 mapTopologyNodes(TopologyType, StateMap, NumNodes, NumNodes) ->
-  io:format("~p~n",[maps:to_list(StateMap)]),
-  createTopology(TopologyType, StateMap, NumNodes);
+%%  io:format("~p~n",[maps:to_list(StateMap)]),
+  TopologyState = createTopology(TopologyType, StateMap, NumNodes);
 
 mapTopologyNodes(TopologyType, StateMap, NumNodes, CountStateMap) ->
   receive
@@ -58,4 +55,40 @@ updateMap(NodesMap, NodePID, N) ->
   Map2.
 
 createTopology(TopologyType, StateMap, NumNodes) ->
+  ProcessList = maps:keys(StateMap),
+  TType = string:lowercase(TopologyType),
+%%  io:fwrite("~p~n", [ProcessList]).
+  case TType of
+    "full" ->
+      io:fwrite("Inside Create~n"),
+      State = createFullTopology(StateMap, NumNodes, ProcessList);
+    "2d" ->
+      State = create2dTopology(StateMap, NumNodes, ProcessList);
+    "line" ->
+      State = createLineTopology(StateMap, NumNodes, ProcessList);
+    "3d" ->
+      State = create3dTopology(StateMap, NumNodes, ProcessList)
+  end,
+  State.
+
+createFullTopology(StateMap, NumNodes, ProcessList) ->
+  io:fwrite("Inside Full Create~n"),
+  Neighbour = findNeighbour(0, StateMap, NumNodes, ProcessList),
   ok.
+
+findNeighbour(NumNodes, StateMap, NumNodes, ProcessList) -> io:fwrite("~p~n",[maps:to_list(StateMap)]);
+findNeighbour(Start, StateMap, NumNodes, ProcessList) ->
+  io:fwrite("Inside Neighbour~n"),
+  {Left, [_|Right]} = lists:split(Start, ProcessList),
+  CurrentNeighbours = Left ++ Right,
+  UpdatedState = maps:update(lists:nth(Start+1, ProcessList), [0, CurrentNeighbours], StateMap),
+  findNeighbour(Start + 1, UpdatedState, NumNodes, ProcessList).
+
+create2dTopology(StateMap, NumNodes, ProcessList) ->
+  erlang:error(not_implemented).
+
+createLineTopology(StateMap, NumNodes, ProcessList) ->
+  erlang:error(not_implemented).
+
+create3dTopology(StateMap, NumNodes, ProcessList) ->
+  erlang:error(not_implemented).
